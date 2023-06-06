701befe70caf894514daee3f8e54c26a31a82fb4
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-06-07 03:06:23 +0530

    show product

--- models/product.js
+++ models/product.js
@@ -1,19 +1,59 @@
-const getdb=require('../util/database').getdb
+///////////////////requier entier object id 
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
 class Product {
-  constructor(title,price,description,imageUrl){
-    this.title=title
-    this.price=price
-this.description=description
-this.imageUrl=imageUrl
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
-  save(){
-const db=getdb()
-db.collection('products').insertOne(this).
-then((result) => {
-  console.log(res)
-}).catch((err)=>{
-console.log("err ")
-})
+
+  save() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+
+  ////////////////////////PRODUCTID Singleproduct
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      /////////////////////if this line added then show product
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
