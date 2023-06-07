382ec81c740f49563b24b172b041da137810b83e
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-06-07 06:04:47 +0530

    add mongodb user emailid name

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect =require("./util/database").mongoConnect
+const user=require('./models/user')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,12 +18,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+  User.findById("647fcee1a9e63f095382b615")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
   next()
 });
 
@@ -34,5 +35,6 @@ app.use(errorController.get404);
 
 mongoConnect(()=>{
 // console.log(client)
+
 app.listen(3000)
 })
--- models/user.js
+++ models/user.js
@@ -1,16 +1,25 @@
-const Sequelize = require('sequelize');
+const mongodb=require('mongodb')
+const getdb=require('../util/database').getDb
+const ObjectId=mongodb.ObjectId
+class User{
+  constructor(username,email){
+this.name=username,
+this.email=email
+  }
+  save(){
+const db=getdb
+return db.collection('users')
+.insertOne(this)
 
-const sequelize = require('../util/database');
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  }
 
-module.exports = User;
+
+    static  findById(userId){
+      const db=getdb
+      return db.collection('users').find({_id:new ObjectId(userId)})
+      
+  }
+}
+
+module.exports=User
