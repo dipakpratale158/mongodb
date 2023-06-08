28a8ed582f041eb671da916a97c9b2a0293d4792
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-06-09 01:45:38 +0530

    autrh

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,15 +18,47 @@ exports.getSignup = (req, res, next) => {
   });
 };
 
+// exports.postLogin = (req, res, next) => {
+//   User.findById('5bab316ce0a7c75f783cb8a8')
+//     .then(user => {
+//       req.session.isLoggedIn = true;
+//       req.session.user = user;
+//       req.session.save(err => {
+//         console.log(err);
+//         res.redirect('/');
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };
+
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          //if password match
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            //callback executge asynchronosly
+            // after saving then redirect
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 };
